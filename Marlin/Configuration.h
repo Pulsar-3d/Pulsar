57db39dc0729a19dc3def60bd8d4eed23e02cfc5
pulsar3d <hola@pulsar3d.com>
2018-04-29 20:05:23 +0200

    Updated configuration.h
    Modified "#define X_PROBE_OFFSET_FROM_EXTRUDER -24"  (not accepts decimals)

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -578,7 +578,7 @@
 //    |           |
 //    O-- FRONT --+
 //  (0,0)
-#define X_PROBE_OFFSET_FROM_EXTRUDER -24.5 // X offset: -left  +right  [of the nozzle]
+#define X_PROBE_OFFSET_FROM_EXTRUDER -24 // X offset: -left  +right  [of the nozzle]
 #define Y_PROBE_OFFSET_FROM_EXTRUDER -3   // Y offset: -front +behind [the nozzle]
 #define Z_PROBE_OFFSET_FROM_EXTRUDER 0   // Z offset: -below +above  [the nozzle]
 
